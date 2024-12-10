1d11d9f33cf7c64d4bfda61ada920ae884ac6e2e
liuhy12345 <90056058+liuhy12345@users.noreply.github.com>
2024-12-09 23:21:46 -0500

    Update run_trained_model.py
    Update new model

--- run_trained_model.py
+++ run_trained_model.py
@@ -1,20 +1,30 @@
 def run_trained_model(X):
-    
-    
-    from sklearn.preprocessing import StandardScaler
-    scaler = StandardScaler()
-    X_scaled = scaler.transform(X)
-    
-    import gdown
-    url = 'https://drive.google.com/file/d/1eGcwmP4SxKkUKKXGlEEv6Y-8rilwtfgw/view?usp=drive_link'
-    output = "new_model.pkl"
-    gdown.download(url, output, fuzzy=True)
-    
-    import pickle
-    with open(output,'rb') as f:
-        model = pickle.load(f)
-    
-    
-    predictions = model(X_scaled)
-    assert predictions.shape == Y.shape
-    return predictions
+  # X is of shape (N, ), where each element is a path string to a WAV file.
+  X_list = []
+  def get_mfcc_features(file_path):
+    data, sr = librosa.load(file_path, sr=None) 
+    data_mfcc = librosa.feature.mfcc(y=data, sr=sr, n_mfcc=13)  
+    audio_data = np.mean(data_mfcc, axis=1)  
+    return audio_data
+  for x in X:
+    if x.endswith(".wav"):
+      mfcc_features = get_mfcc_features(x)
+      X_list.append(mfcc_features)
+  
+  X_list = np.array(X_list)
+  import gdown
+  url = 'https://drive.google.com/file/d/1LaCFZIrNUzSbKywjSSPI0-jtvDUd5ny_/view?usp=drive_link'
+  
+  output = "new_model.pkl"
+  gdown.download(url, output, fuzzy=True)
+
+  
+  import pickle
+  import sklearn
+  with open(output,'rb') as f:
+    model = pickle.load(f)
+
+  
+  predictions = model.predict(X_list)
+  assert predictions.shape == Y.shape
+  return predictions
